b2c3ff1ebb0aba3bc8c593cac446b79721180fb3
hania-git-sketch <abidhaniya34@gmail.com>
2026-03-03 12:18:51 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+﻿// Mobile Menu Toggle
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("nav");
+
+hamburger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+// Reserve Button
+function reserveTable() {
+    alert("Thank you! Our team will contact you soon ☕💜");
+}
+
+// Scroll Reveal Animation
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+    const reveals = document.querySelectorAll(".reveal");
+
+    reveals.forEach(element => {
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+        const visible = 100;
+
+        if (elementTop < windowHeight - visible) {
+            element.classList.add("active");
+        }
+    });
+}
